701ce22f7d52e7626adc4ad0120ccd8080db0a62
ValentinaZussino <vzussino@protonmail.com>
2022-11-28 15:24:45 +0100

    prova con table

--- index.php
+++ index.php
@@ -48,6 +48,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <title>Hotels</title>
 </head>
 <body>
@@ -59,5 +61,30 @@
         }
     }
     ?>
+    <!-- prova con table -->
+    <br><br>
+    <table class="table table-hover">
+        <thead>
+            <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Description</th>
+            <th scope="col">Parking</th>
+            <th scope="col">Vote</th>
+            <th scope="col">Distance from center</th>
+            </tr>
+        </thead>
+
+        <?php foreach($hotels as $hotel){ ?>
+        <tbody>
+            <tr>
+                <th scope="row"><?php echo $hotel['name'] ?></th>
+                <td><?php echo $hotel['description'] ?></td>
+                <td><?php echo $hotel['parking'] ?></td>
+                <td><?php echo $hotel['vote'] ?></td>
+                <td><?php echo $hotel['distance_to_center'] ?></td>
+            </tr>
+        </tbody>
+        <?php } ?>
+    </table>
 </body>
 </html>
