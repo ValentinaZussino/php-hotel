15efae6738ee5ba75c691fb139948645ff3b85d7
ValentinaZussino <vzussino@protonmail.com>
2022-11-28 16:09:51 +0100

    breve impostaz layout

--- index.php
+++ index.php
@@ -50,41 +50,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/style.css">
     <title>Hotels</title>
 </head>
 <body>
-    <h1>Hotels</h1>
-    <?php 
-    foreach($hotels as $hotel){
-        foreach($hotel as $key=>$value){
-            echo '<div>'. $key . ' : ' . $value .'</div>' ;
+    <div class="vz_container">
+        <h1>Hotels</h1>
+        <!-- <?php 
+        foreach($hotels as $hotel){
+            foreach($hotel as $key=>$value){
+                echo '<div>'. $key . ' : ' . $value .'</div>' ;
+            }
         }
-    }
-    ?>
-    <!-- prova con table -->
-    <br><br>
-    <table class="table table-hover">
-        <thead>
-            <tr>
-            <th scope="col">Name</th>
-            <th scope="col">Description</th>
-            <th scope="col">Parking</th>
-            <th scope="col">Vote</th>
-            <th scope="col">Distance from center</th>
-            </tr>
-        </thead>
+        ?> -->
+        <!-- prova con table -->
+        <br><br>
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Description</th>
+                <th scope="col">Parking</th>
+                <th scope="col">Vote</th>
+                <th scope="col">Distance from center</th>
+                </tr>
+            </thead>
 
-        <?php foreach($hotels as $hotel){ ?>
-        <tbody>
-            <tr>
-                <th scope="row"><?php echo $hotel['name'] ?></th>
-                <td><?php echo $hotel['description'] ?></td>
-                <td><?php echo $hotel['parking'] ?></td>
-                <td><?php echo $hotel['vote'] ?></td>
-                <td><?php echo $hotel['distance_to_center'] ?></td>
-            </tr>
-        </tbody>
-        <?php } ?>
-    </table>
+            <?php foreach($hotels as $hotel){ ?>
+            <tbody>
+                <tr>
+                    <td scope="row"><?php echo $hotel['name'] ?></td>
+                    <td><?php echo $hotel['description'] ?></td>
+                    <td><?php echo $hotel['parking'] ?></td>
+                    <td><?php echo $hotel['vote'] ?></td>
+                    <td><?php echo $hotel['distance_to_center'] ?></td>
+                </tr>
+            </tbody>
+            <?php } ?>
+        </table>
+    </div>
 </body>
 </html>
